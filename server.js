64ddb05cc5ddadfd38a07682ed35342cacb6cbb3
Talia <talia@Preethi-Srinivasans-MacBook-Pro-2.local>
2020-06-29 16:36:07 -0700

    mailchimp integration

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+var request = require('superagent');
 var express = require('express');
 var app = express();
 var parser = require('body-parser');
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(parser.urlencoded({ extended: false}))
 app.use(parser.json())
+app.use (express.static('views'));
+
 
 //index page
 app.get('/', function(req,res) {
@@ -38,7 +42,7 @@ app.get('/contactme',function(req,res){
     console.log('user accessing Home page');
 });
 
-app.post('/contactsubmit',function(req,res){
+/*app.post('/contactsubmit',function(req,res){
     var data = {
         first : req.body.fname,
         last : req.body.lname,
@@ -52,6 +56,33 @@ app.post('/contactsubmit',function(req,res){
     });
     //res.json(student);
      
+});*/
+
+var mailchimpInstance   = 'us10',
+    listUniqueId        = '6f7a38258d',
+    mailchimpApiKey     = '83e5018de51b8a18f02a4402b6208f15-us10';
+
+app.post('/contactsubmit', function(req, res) {
+	request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer.from('any:' + mailchimpApiKey ).toString('base64'))
+        .send({
+          'email_address': req.body.email,
+          'status': 'subscribed',
+          'merge_fields': {
+            'FNAME': req.body.fname,
+            'LNAME': req.body.lname
+}
+ })
+ 		.end(function(err, response) {
+ 			  //console.log(response);
+              if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+                res.send('Signed Up!');
+              } else {
+                res.send('Sign Up Failed :(');
+              }
+          });
 });
 
 
@@ -64,6 +95,7 @@ app.get('/about', function(req,res) {
 /*app.get('/', function (req, res) {
    res.send('Hello World');
 })*/
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
